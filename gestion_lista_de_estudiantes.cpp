65c642cd3cac9c1428cac23454681d38ee2b4886
av3lar <00089625@uca.edu.com>
2026-01-16 18:26:02 -0600

    add: codeLista

--- gestion_lista_de_estudiantes.cpp
+++ gestion_lista_de_estudiantes.cpp
@@ -24,9 +24,86 @@ public:
     }
 };
 
+
+class Nodo{ 
+
+    public:
+    Estudiante estudiante;
+    Nodo* siguiente;
+
+    Nodo(Estudiante e) : estudiante(e) { 
+        siguiente = nullptr;
+    }
+};
+
+
+class ListaEnlazada { 
+    private: Nodo* head;
+
+    float sumarNotasRecursivo(Nodo* nodo) { 
+        if(nodo==nullptr)
+        return 0;
+        return nodo ->estudiante.obtenerNotaFinal() + sumarNotasRecursivo(nodo->siguiente);
+    }
+
+    int contarNodosRecursivo(Nodo* nodo) { 
+        if(nodo==nullptr)
+        return 0;
+        return 1 + contarNodosRecursivo(nodo->siguiente);
+    }
+    public: 
+    ListaEnlazada(){ 
+        head = nullptr;
+    }
+
+    void agregarEstudiante(string nombre, float nota) { 
+        Estudiante nuevo(nombre, nota);
+        Nodo* nuevoNodo = new Nodo(nuevo);
+
+        if(head == nullptr) {
+            head = nuevoNodo;
+        } else { 
+            Nodo* actual = head;
+            while(actual->siguiente != nullptr) { 
+                actual = actual->siguiente;
+            }
+            actual->siguiente = nuevoNodo;
+        }
+    }
+
+
+    void imprimirLista() {
+
+        Nodo* actual = head;
+        while(actual!=nullptr) {
+            actual->estudiante.imprimir();
+            actual = actual ->siguiente;
+        }
+    }
+
+     float calcularPromedioRecursivo() {
+        int total = contarNodosRecursivo(head);
+        if (total == 0) return 0;
+
+        float suma = sumarNotasRecursivo(head);
+        return suma / total;
+    }
+
+};
+
 int main() {
     
-    
+    ListaEnlazada lista;
+
+    lista.agregarEstudiante("Daniel", 8);
+    lista.agregarEstudiante("Medrano", 7.5);
+    lista.agregarEstudiante("Calles", 5.4);
+
+    cout<<" Lista de estudiantes\n";
+    lista.imprimirLista();
+
+    float promedio = lista.calcularPromedioRecursivo();
+    cout<< " El promedio de notas es: " << promedio << " \n";
     
     return 0;
 }
